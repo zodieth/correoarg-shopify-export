74407e95130bad5276e691d67f8cbe0d1cf21e1d
Mateo Iglesias <contact.prodzodi@gmail.com>
2023-11-29 12:43:24 -0300

    build

--- app/components/fileUploader.jsx
+++ app/components/fileUploader.jsx
@@ -63,105 +63,91 @@ const FileUploader = () => {
       .replace(/ü/gi, "u"); // Reemplazar ü por u
   };
 
-  const processData = (data) => {
-    // Aquí puedes realizar el procesamiento de datos necesario
-    // Por ejemplo, cambiar el formato de columnas, agregar o quitar datos, etc.
+  const processData = async (data) => {
+    const newData = await Promise.all(
+      data.map(async (item) => {
+        const cleanedPhoneNumber = cleanPhoneNumber(item["Shipping Phone"]);
+        const cleanedCodpostal = cleanPostalCode(item["Shipping Zip"]);
+        const shippingCity = removeAccentsAndSpecialChars(
+          item["Shipping City"]
+        );
+        const shippingAddress = removeAccentsAndSpecialChars(
+          item["Shipping Address1"]
+        );
+        const shippingName = removeAccentsAndSpecialChars(
+          item["Shipping Name"]
+        );
 
-    // Mapeo de columnas
-    const newData = data.map(async (item) => {
-      const cleanedPhoneNumber = await cleanPhoneNumber(item["Shipping Phone"]);
+        return {
+          "tipo_producto(obligatorio)": "CP",
+          "largo(obligatorio en CM)": "",
+          "ancho(obligatorio en CM)": "",
+          "altura(obligatorio en CM)": "",
+          "peso(obligatorio en KG)": "",
+          "valor_del_contenido(obligatorio en pesos argentinos)": "",
+          "provincia_destino(obligatorio)": item["Shipping Province"],
+          "sucursal_destino(obligatorio solo en caso de no ingresar localidad de destino)":
+            "",
+          "localidad_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
+            shippingCity,
+          "calle_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
+            shippingAddress,
+          "altura_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
+            "",
+          "piso(opcional solo en caso de no ingresar sucursal de destino)": "",
+          "dpto(opcional solo en caso de no ingresar sucursal de destino)": "",
+          "codpostal_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
+            cleanedCodpostal,
+          "destino_nombre(obligatorio)": shippingName,
+          "destino_email(obligatorio, debe ser un email valido)": item["Email"],
+          "cod_area_tel(opcional)": "",
+          "tel(opcional)": "",
+          "cod_area_cel(obligatorio)": "54",
+          "cel(obligatorio)": cleanedPhoneNumber,
+        };
+      })
+    );
 
-      const cleanedCodpostal = await cleanPostalCode(item["Shipping Zip"]);
-
-      const shippingCity = await removeAccentsAndSpecialChars(
-        item["Shipping City"]
-      );
-      const shippingAddress = await removeAccentsAndSpecialChars(
-        item["Shipping Address1"]
-      );
-      const shippingName = await removeAccentsAndSpecialChars(
-        item["Shipping Name"]
-      );
-
-      return {
-        "tipo_producto(obligatorio)": "CP",
-        "largo(obligatorio en CM)": "",
-        "ancho(obligatorio en CM)": "",
-        "altura(obligatorio en CM)": "",
-        "peso(obligatorio en KG)": "",
-        "valor_del_contenido(obligatorio en pesos argentinos)": "",
-        "provincia_destino(obligatorio)": item["Shipping Province"],
-        "sucursal_destino(obligatorio solo en caso de no ingresar localidad de destino)":
-          "",
-        "localidad_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
-          shippingCity,
-        "calle_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
-          shippingAddress,
-        "altura_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
-          "",
-        "piso(opcional solo en caso de no ingresar sucursal de destino)": "",
-        "dpto(opcional solo en caso de no ingresar sucursal de destino)": "",
-        "codpostal_destino(obligatorio solo en caso de no ingresar sucursal de destino)":
-          cleanedCodpostal,
-        "destino_nombre(obligatorio)": shippingName,
-        "destino_email(obligatorio, debe ser un email valido)": item["Email"],
-        "cod_area_tel(opcional)": "",
-        "tel(opcional)": "",
-        "cod_area_cel(obligatorio)": "54",
-        "cel(obligatorio)": cleanedPhoneNumber,
-      };
-    });
-
-    // Crear un nuevo libro de trabajo (workbook)
     const workbook = XLSX.utils.book_new();
-
-    // Crear una hoja de trabajo (worksheet)
     const worksheet = XLSX.utils.json_to_sheet(newData);
-
-    // Agregar la hoja de trabajo al libro de trabajo
     XLSX.utils.book_append_sheet(workbook, worksheet, "Nueva Hoja");
-
-    // Guardar el libro de trabajo como un archivo Excel
     XLSX.writeFile(workbook, "estearchivotecambialavida.xlsx");
-
-    // O bien, puedes devolver el libro de trabajo si lo necesitas para otro propósito
-    // return workbook;
 
     return workbook;
   };
 
   const generateXLSX = (data) => {
     if (!Array.isArray(data)) {
-      data = [data]; // Convierte a un array si no lo es
+      data = [data];
     }
 
     const worksheet = XLSX.utils.json_to_sheet(data);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-    XLSX.writeFile(workbook, "formatted_data.xlsx");
+    // XLSX.writeFile(workbook, "formatted_data.xlsx");
   };
 
   return (
     <div className="mx-10 md:mx-2 flex items-center justify-center h-screen">
-      <div class="max-w-2xl mx-auto">
+      <div className="max-w-2xl mx-auto">
         <label
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          for="file_input"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          htmlFor="file_input"
         >
           Subir archivo{" "}
         </label>
         <input
           onChange={handleFileChange}
-          class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           id="file_input"
           type="file"
         />
 
-        <p class="mt-5">
+        <p className="mt-5">
           Con esto te ahorro 30 min de estar cargando manualmente el excel de
           Correo Argentino y Shopify, cargá el archivo order_exports y disfrutá
           pa,{" "}
-          <a class="text-blue-600 hover:underline" href="#" target="_blank">
+          <a className="text-blue-600 hover:underline" href="#" target="_blank">
             Mateo
           </a>
           .
